ad3013c92cd29ab536656dd0c338b40baa2b8e9b
Supun Nethsara <supunmax663@gmail.com>
2025-08-01 06:13:49 -0700

    installed resact router dom

---
+++ src/firebaseConfig.js
@@ -0,0 +1,24 @@
+// firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDr2D65v-Pmc_eyn3qs4RASqya6cNpfUs4",
+  authDomain: "my-portfolio-c0afe.firebaseapp.com",
+  projectId: "my-portfolio-c0afe",
+  storageBucket: "my-portfolio-c0afe.firebasestorage.app",
+  messagingSenderId: "711951189543",
+  appId: "1:711951189543:web:524b062dd4cbd2824e7002",
+  measurementId: "G-3VY08Z5SXE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export Firebase services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
